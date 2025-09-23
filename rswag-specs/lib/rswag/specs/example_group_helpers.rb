9b3bd8c6b718a2478bf8cd9c84b2ce8389890946
Jay Vercellone <jay@pipedream.com>
2025-09-23 20:50:34 -0300

    Skip assertions in `run_test!`
    Create a new method called `run_test_fake!` that performs the OpenAPI
    spec generation of a particular response, but without checking the
    test assertions. This is useful when we want to generate a custom
    response in the spec that does not follow the actual API
    behaviour (e.g. when dealing with redirects to resources served by
    other services).

--- rswag-specs/lib/rswag/specs/example_group_helpers.rb
+++ rswag-specs/lib/rswag/specs/example_group_helpers.rb
@@ -10,14 +10,14 @@ module Rswag
         describe(template, metadata, &block)
       end
 
-      [:get, :post, :patch, :put, :delete, :head, :options, :trace].each do |verb|
+      %i[get post patch put delete head options trace].each do |verb|
         define_method(verb) do |summary, **metadata, &block|
           api_metadata = { operation: { verb: verb, summary: summary } }.deep_merge(metadata)
           describe(verb, **api_metadata, &block)
         end
       end
 
-      [:operationId, :deprecated, :security].each do |attr_name|
+      %i[operationId deprecated security].each do |attr_name|
         define_method(attr_name) do |value|
           metadata[:operation][attr_name] = value
         end
@@ -39,16 +39,14 @@ module Rswag
       end
 
       # These are array properties - note the splat operator
-      [:tags, :consumes, :produces, :schemes].each do |attr_name|
+      %i[tags consumes produces schemes].each do |attr_name|
         define_method(attr_name) do |*value|
           metadata[:operation][attr_name] = value
         end
       end
 
       def parameter(attributes)
-        if attributes[:in] && attributes[:in].to_sym == :path
-          attributes[:required] = true
-        end
+        attributes[:required] = true if attributes[:in] && attributes[:in].to_sym == :path
 
         if metadata.key?(:operation)
           metadata[:operation][:parameters] ||= []
@@ -60,14 +58,14 @@ module Rswag
       end
 
       def request_body_example(value:, summary: nil, name: nil)
-        if metadata.key?(:operation)
-          metadata[:operation][:request_examples] ||= []
-          example = { value: value }
-          example[:summary] = summary if summary
-          # We need the examples to have a unique name for a set of examples, so just make the name the length if one isn't provided.
-          example[:name] = name || metadata[:operation][:request_examples].length()
-          metadata[:operation][:request_examples] << example
-        end
+        return unless metadata.key?(:operation)
+
+        metadata[:operation][:request_examples] ||= []
+        example = { value: value }
+        example[:summary] = summary if summary
+        # We need the examples to have a unique name for a set of examples, so just make the name the length if one isn't provided.
+        example[:name] = name || metadata[:operation][:request_examples].length
+        metadata[:operation][:request_examples] << example
       end
 
       def response(code, description, metadata = {}, &block)
@@ -90,17 +88,16 @@ module Rswag
       # rspec-core ExampleGroup
       def examples(examples = nil)
         return super() if examples.nil?
+
         # should we add a deprecation warning?
         examples.each_with_index do |(mime, example_object), index|
           example(mime, "example_#{index}", example_object)
         end
       end
 
-      def example(mime, name, value, summary=nil, description=nil)
+      def example(mime, name, value, summary = nil, description = nil)
         # Todo - move initialization of metadata somewhere else.
-        if metadata[:response][:content].blank?
-          metadata[:response][:content] = {}
-        end
+        metadata[:response][:content] = {} if metadata[:response][:content].blank?
 
         if metadata[:response][:content][mime].blank?
           metadata[:response][:content][mime] = {}
@@ -126,7 +123,7 @@ module Rswag
       # @param options [Hash] options to pass to the `it` method
       # @param &block [Proc] you can make additional assertions within that block
       # @return [void]
-      def run_test!(description = nil, *args, **options, &block)
+      def run_test!(description = nil, *args, assert: true, **options, &block)
         # rswag metadata value defaults to true
         options[:rswag] = true unless options.key?(:rswag)
 
@@ -138,7 +135,7 @@ module Rswag
           end
 
           it description, *args, **options do
-            assert_response_matches_metadata(metadata)
+            assert_response_matches_metadata(metadata) if assert
             block.call(response) if block_given?
           end
         else
@@ -147,10 +144,25 @@ module Rswag
           end
 
           it description, *args, **options do |example|
-            assert_response_matches_metadata(example.metadata, &block)
+            assert_response_matches_metadata(example.metadata, &block) if assert
             example.instance_exec(response, &block) if block_given?
           end
         end
+      end
+
+      #
+      # Perform request without asserting response matches swagger definitions
+      #
+      # This is useful when you want to generate the swagger documentation
+      # but don't want to validate the response.
+      #
+      # @param description [String] description of the test
+      # @param args [Array] arguments to pass to the `it`
+      # @param options [Hash] options to pass to the `it`
+      # @param &block [Proc] you can make additional assertions within that block
+      # @return [void]
+      def run_test_fake!(description = nil, *args, **options, &block)
+        run_test!(description, *args, assert: false, **options, &block)
       end
     end
   end
